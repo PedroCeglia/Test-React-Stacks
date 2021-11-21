ceba2c4ed00e92fa92a7903407f33348f37cd704
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-21 17:50:26 -0300

    Add React Redux Provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+// Import React Router Dom
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
+
+// Import Redux
+import store from './Redux/Store';
+
+// Import React Redux
+import { Provider } from 'react-redux';
+
+// Import Widgets
+import Home from './Pages/Home'
+import Contact from './Pages/Contact';
+import Help from './Pages/Help';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Provider store={store}>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home/>}>
+
+              <Route path='/contact' element={<Contact/>}/>
+              
+              <Route path='/help' element={<Help/>}>
+                <Route path='/help/name' element={<h1>Nome</h1>}/>
+                <Route path='/help/email' element={<h1>Email</h1>}/>
+                <Route path='/help/foto' element={<h1>Foto</h1>}/>
+              </Route>
+            </Route>
+        </Routes>
+    </BrowserRouter>
+  </Provider>
   );
 }
 
